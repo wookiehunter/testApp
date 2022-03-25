c62531ab709634882b0dac07d54971d5018d6b17
Steven Godson <steven@xenos-design.co.uk>
2022-03-25 17:03:21 +0000

    image and input added

--- src/features/intro.js
+++ src/features/intro.js
@@ -1,23 +1,51 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, TextInput, Image, StyleSheet} from 'react-native';
 import {colours} from '../utils/colours.js';
 import {sizes} from '../utils/sizes.js';
 
 export const Intro = () => (
-  <View style={styles.container}>
-    <Text style={styles.text}>Hello! is it me you're looking for...?'</Text>
-  </View>
+  <>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello! is it me you're looking for...?'</Text>
+    </View>
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Tell me the truth...!"
+        placeholderTextColor={colours.green}
+      />
+    </View>
+    <View style={styles.container}>
+      <Image
+        style={styles.image}
+        source={require('../images/typeWriter.jpg')}
+      />
+    </View>
+  </>
 );
 
 const styles = StyleSheet.create({
   container: {
-    flex: 0.3,
+    flex: 1,
     paddingTop: sizes.lg,
+    alignItems: 'center',
   },
   text: {
     color: colours.orange,
     fontWeight: '600',
     fontSize: sizes.md,
     textAlign: 'center',
+  },
+  input: {
+    height: 50,
+    width: 300,
+    padding: 15,
+    borderColor: colours.orange,
+    borderWidth: 2,
+  },
+  image: {
+    width: 500,
+    height: 500,
+    marginTop: 20,
   },
 });
